4a44433f03f7d5ebb944b874f1486f28674011c5
Yunfly <120562638@qq.com>
2017-11-11 13:08:13 +0800

    update ctx

--- server-egg/app/controller/home.js
+++ server-egg/app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+    this.ctx.body = 'hi, 秦飞云的个人网站正在搭建中...';
   }
 }
 
